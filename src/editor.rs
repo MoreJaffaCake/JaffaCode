d86de3b09feadd616ecfc0f79c2654f2af0f10bd
JaffaCake <211059585+MoreJaffaCake@users.noreply.github.com>
2025-05-12 11:50:53 +0200

    Don't update char_idx on every move, only on edit

--- src/editor.rs
+++ src/editor.rs
@@ -7,20 +7,24 @@ use ropey::*;
 #[derive(Debug)]
 pub struct Editor {
     rope: Rope,
-    trailing_spaces: usize,
-    char_idx: usize,
+    position: Option<Position>,
     cur_y: u16,
     cur_x: u16,
     white_spaces: String,
     vlines: visual_lines::VisualLines,
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Position {
+    trailing_spaces: usize,
+    char_idx: usize,
+}
+
 impl Editor {
     pub fn new(initial_text: &str) -> Self {
         let mut instance = Self {
             rope: Rope::from_str(initial_text),
-            trailing_spaces: 0,
-            char_idx: 0,
+            position: None,
             cur_y: 0,
             cur_x: 0,
             white_spaces: std::iter::repeat(' ').take(200).collect::<String>(),
@@ -34,16 +38,46 @@ impl Editor {
         instance
     }
 
-    pub fn insert_char(&mut self, c: char) {
-        self.vlines.insert(self.trailing_spaces + 1);
-        if self.trailing_spaces > 0 {
-            self.rope
-                .insert(self.char_idx, &self.white_spaces[..self.trailing_spaces]);
-            self.char_idx += self.trailing_spaces;
-            self.trailing_spaces = 0;
+    fn position(&mut self) -> &mut Position {
+        if self.position.is_none() {
+            let line = self.vlines.cursor();
+            let mut char_idx = self.rope.byte_to_char(line.start);
+            let trailing_spaces;
+            let line_len = line.slice(&self.rope).len_chars().saturating_sub(1);
+            if line_len >= self.cur_x as usize {
+                char_idx += self.cur_x as usize;
+                trailing_spaces = 0;
+            } else {
+                char_idx += line_len;
+                trailing_spaces = self.cur_x as usize - line_len;
+            }
+            self.position = Some(Position {
+                trailing_spaces,
+                char_idx,
+            });
         }
-        self.rope.insert_char(self.char_idx, c);
-        self.char_idx += 1;
+
+        self.position.as_mut().unwrap()
+    }
+
+    #[inline(always)]
+    fn clear_position(&mut self) {
+        self.position.take();
+    }
+
+    pub fn insert_char(&mut self, c: char) {
+        let Position {
+            trailing_spaces,
+            mut char_idx,
+        } = *self.position();
+        self.vlines.insert(trailing_spaces + 1);
+        if trailing_spaces > 0 {
+            self.rope
+                .insert(char_idx, &self.white_spaces[..trailing_spaces]);
+            char_idx += trailing_spaces;
+        }
+        self.rope.insert_char(char_idx, c);
+        self.position().char_idx += 1;
         if c == '\n' {
             self.vlines.move_cursor_next();
             self.cur_y += 1;
@@ -51,23 +85,29 @@ impl Editor {
         } else {
             self.cur_x += 1;
         }
+        let pos = self.position();
+        pos.char_idx = char_idx + 1;
+        pos.trailing_spaces = 0;
     }
 
     pub fn delete_char_forward(&mut self) {
-        if self.rope.try_remove(self.char_idx..=self.char_idx).is_ok() {
+        let char_idx = self.position().char_idx;
+        if self.rope.try_remove(char_idx..=char_idx).is_ok() {
             self.vlines.remove(1);
         }
     }
 
     pub fn delete_char_backward(&mut self) {
+        let Position { mut char_idx, .. } = *self.position();
         if self.cur_x > 0 {
-            self.char_idx -= 1;
-            self.rope.remove(self.char_idx..=self.char_idx);
+            char_idx -= 1;
+            self.rope.remove(char_idx..=char_idx);
             self.vlines.remove(1);
             self.cur_x -= 1;
-        } else if self.char_idx > 0 {
-            self.char_idx -= 1;
-            self.rope.remove(self.char_idx..=self.char_idx);
+            self.position().char_idx = char_idx;
+        } else if char_idx > 0 {
+            char_idx -= 1;
+            self.rope.remove(char_idx..=char_idx);
             self.vlines.remove(1);
             self.vlines.move_cursor_prev();
             let line_len = self
@@ -79,6 +119,7 @@ impl Editor {
             let line = self.vlines.merge_next();
             self.cur_y -= 1;
             self.cur_x = line_len as _;
+            self.position().char_idx = char_idx;
         }
     }
 
@@ -86,52 +127,26 @@ impl Editor {
         if self.cur_y > 0 {
             self.cur_y -= 1;
             self.vlines.move_cursor_prev();
-            let line = self.vlines.cursor();
-            let line_len = line.slice(&self.rope).len_chars().saturating_sub(1);
-            if line_len >= self.cur_x as usize {
-                self.char_idx = self.rope.byte_to_char(line.start) + self.cur_x as usize;
-            } else {
-                self.char_idx = self.rope.byte_to_char(line.end) - 1;
-                self.trailing_spaces = self.cur_x as usize - line_len + 1;
-            }
+            self.clear_position();
         }
     }
 
     pub fn move_cursor_down(&mut self) {
         self.cur_y += 1;
         self.vlines.move_cursor_next();
-        let line = self.vlines.cursor();
-        let line_len = line.slice(&self.rope).len_chars().saturating_sub(1);
-        self.char_idx = self.rope.byte_to_char(line.start);
-        if line_len >= self.cur_x as usize {
-            self.char_idx += self.cur_x as usize;
-            self.trailing_spaces = 0;
-        } else {
-            self.char_idx += line_len;
-            self.trailing_spaces = self.cur_x as usize - line_len + 1;
-        }
+        self.clear_position();
     }
 
     pub fn move_cursor_left(&mut self) {
         if self.cur_x > 0 {
             self.cur_x -= 1;
-            if self.trailing_spaces > 0 {
-                self.trailing_spaces -= 1;
-            } else {
-                self.char_idx -= 1;
-            }
+            self.clear_position();
         }
     }
 
     pub fn move_cursor_right(&mut self) {
         self.cur_x += 1;
-        let line = self.vlines.cursor();
-        let line_len = line.slice(&self.rope).len_chars().saturating_sub(1);
-        if line_len >= self.cur_x as usize {
-            self.char_idx += 1;
-        } else {
-            self.trailing_spaces += 1;
-        }
+        self.clear_position();
     }
 
     pub fn get_display_lines(&self) -> impl Iterator<Item = RopeSlice> {
